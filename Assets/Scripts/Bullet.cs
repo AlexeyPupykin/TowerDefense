5898715197e2e8fd8873a7d59305a270e2ee7794
AlexeyPupykin <mymailvrn98@gmail.com>
2022-01-09 22:15:45 +0300

    created GameManager

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,17 +5,15 @@ public class Bullet : MonoBehaviour
     private Transform target;
 
     public float speed = 70f;
+
+    public int damage = 50;
+
     public float explosionRadius = 0f;
     public GameObject impactEffect;
 
     public void Seek(Transform _target)
     {
         target = _target;
-    }
-
-    void Start()
-    {
-        
     }
 
     void Update()
@@ -44,7 +42,7 @@ public class Bullet : MonoBehaviour
     {
         GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(effectIns, 2f);
-        Destroy(target.gameObject);
+        //Destroy(target.gameObject);
 
         if(explosionRadius > 0f)
         {
@@ -72,6 +70,10 @@ public class Bullet : MonoBehaviour
 
     void Damage(Transform enemy)
     {
-        Destroy(enemy.gameObject);
+        Enemy e = enemy.GetComponent<Enemy>();
+        if(e != null)
+        {
+            e.TakeDamage(damage);
+        }
     }
 }
