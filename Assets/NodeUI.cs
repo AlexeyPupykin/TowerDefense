89a09fe7108639670f33ea0555ee28c6fc2055b0
AlexeyPupykin <mymailvrn98@gmail.com>
2022-01-10 05:27:02 +0300

    show real price for upgrade

--- Assets/NodeUI.cs
+++ Assets/NodeUI.cs
@@ -1,10 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NodeUI : MonoBehaviour
 {
     public GameObject ui;
+
+    public Text upgradeCost;
+    public Button upgradeButton;
 
     private Node target;
 
@@ -13,6 +15,18 @@ public class NodeUI : MonoBehaviour
         target = _target;
 
         transform.position = new Vector3(target.GetBuildPosition().x, target.GetBuildPosition().y + 9f, target.GetBuildPosition().z);
+
+        if(!target.isUpgraded)
+        {
+            upgradeCost.text = "$" + target.turretBlueprint.upgradeCost;
+            upgradeButton.interactable = true;
+        }
+        else
+        {
+            upgradeCost.text = "DONE";
+            upgradeButton.interactable = false;
+        }
+
         ui.SetActive(true);
     }
 
